65e7a31ee0885d4db03e074509360c67f1aafaca
PENEKhun <penekhun@gmail.com>
2022-08-16 06:48:31 +0900

    feat: Custom Exception 적용

---
+++ src/main/java/com/bulkup/health/config/exception/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.bulkup.health.config.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.UnexpectedTypeException;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(CustomException.class)
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e){
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+
+    @ExceptionHandler(UnexpectedTypeException.class)
+    protected ResponseEntity<ErrorResponse> handleUnexpectedTypeException(final UnexpectedTypeException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST.value())
+                .body(new ErrorResponse("C000", e.getMessage(), "UnexpectedTypeException"));
+    }
+
+}
