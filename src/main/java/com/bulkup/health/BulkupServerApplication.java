7d5c7e888873fa7a12c5ea4157a98b82bd158eb3
PENEKhun <penekhun@gmail.com>
2022-08-16 06:53:33 +0900

    etc: BCryptPassword 빈 등록

--- src/main/java/com/bulkup/health/BulkupServerApplication.java
+++ src/main/java/com/bulkup/health/BulkupServerApplication.java
@@ -3,6 +3,8 @@ package com.bulkup.health;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableJpaAuditing
 @SpringBootApplication
@@ -11,5 +13,8 @@ public class BulkupServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(BulkupServerApplication.class, args);
     }
-
+    @Bean
+    public BCryptPasswordEncoder encodePwd() {
+        return new BCryptPasswordEncoder();
+    }
 }
